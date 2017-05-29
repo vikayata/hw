c63be55d257192c1992d007129574e04c4d54187
Vika Yata <vika.yasen@gmail.com>
2017-05-29 06:55:02 +0300

    Updating last hw part 1

--- problem3.1.py
+++ problem3.1.py
@@ -1,8 +1,10 @@
 import datetime as dt
 
-today = dt.date.today()
-#print(type(today))
+def time_to_ny():
+    today = dt.date.today()
+    #print(type(today))
 
-new_year = dt.date(today.year, 12, 31)
-time_to_new_year = abs(new_year - today)
-print('До Нового года осталось', time_to_new_year.days, 'дней.')
+    new_year = dt.date(today.year, 12, 31)
+    return abs(new_year - today).days
+
+print('До Нового года осталось', time_to_ny(), 'дней.')
