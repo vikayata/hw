3338f516b6a5a521b9535fd33ed43e97ea9c7774
Vika Yata <vika.yasen@gmail.com>
2017-06-09 14:25:13 +0300

    Добавлен таймер в декоратор задержки функции

--- pause_func.py
+++ pause_func.py
@@ -1,21 +1,27 @@
 # ДЕКОРАТОР ПРИОСТАНОВКИ ФУНКЦИИ
 
 import time
-
+   
 def pause(n):
+    
+    def timer(n):
+        while n:
+            print('{} сек'.format(n))
+            time.sleep(1)
+            n -= 1
+
     def decorator(func):
         def wrapper(*args, **kwargs):
-            print('''
-Запрос на выполнение функции {} принят. Функция будет выполнена через {} секунд. 
-'''.format(func.__name__, n))
-            time.sleep(n)
+            print('Запрос на выполнение функции {} принят. Функция будет выполнена через: '.format(func.__name__))
+            timer(n)
+                        
             return func(*args, **kwargs)
 
         return wrapper
 
     return decorator
 
-@pause(10)
+@pause(15)
 def random_func():
     print('Hello world!')
 
